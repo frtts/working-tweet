d139a1a1da57914dccf45cdee917babee7adb0d9
furutatu <nyannyanboo@gmail.com>
2011-10-05 18:44:57 +0900

    add files

--- wt_run.rb
+++ wt_run.rb
@@ -5,15 +5,8 @@ require 'twitter'
 CONSUMERKEY = "EkZzmg9e1urB2pdMusxuQ"
 CONSUMERSECRET = "LbYynipCQq8nXoU0yzWhoIgQl6ykr2zjufoe4PkKAc"
 
-consumer = OAuth::Consumer.new(CONSUMERKEY, CONSUMERSECRET, :site => "http://api.twitter.com")
-
-oauth_verifier = "6475586"
-access_token = consumer.get_access_token
-access_tok = access_token.token
-access_secret = access_token.secret
-
-#access_tok = "20989679-vRVegGOaP6sTC1C8oLdZG4vwDI3xvTYk79rHd6Km9"
-#access_secret = "irhW5hVeuXMkhdYlPaE6DrsYi0fu3lUM8w6cNMaGg"
+access_tok = "20989679-vRVegGOaP6sTC1C8oLdZG4vwDI3xvTYk79rHd6Km9"
+access_secret = "irhW5hVeuXMkhdYlPaE6DrsYi0fu3lUM8w6cNMaGg"
 
 Twitter.configure do |config|
 	config.consumer_key = CONSUMERKEY
@@ -22,24 +15,39 @@ Twitter.configure do |config|
 	config.oauth_token_secret = access_secret
 end
 
+#Twitter.methods.sort
+#Twitter.user
+#$KCONF = "UTF-8"
+#usr = Twitter.user
+#usr.keys
+#usr[:profile_sidebar_fill_color]
+#usr[:name]
+#puts usr[:name]
+#usr.keys.sort
+#p Twitter.update("かみなりだーーー")
+
 $KCONF = "UTF-8"
 command = ARGV.shift.chomp
 if (command == "timeline" or command == "tl")
 	#puts "status id\t\tscreen name\ttweet"
 	if (ARGV.shift != nil)
 		Twitter.home_timeline.each do |tl|
-			puts "%s\t%s\t%s"%[tl.id, tl.user.screen_name, tl.text]
+			puts "%s %s\n%s\n"%[tl.id, tl.user.screen_name, tl.text]
 		end
 	else
 		Twitter.friends_timeline.each do |tl|
-			puts "%s\t%s\t%s"%[tl.id, tl.user.screen_name, tl.text]
+			puts "%s %s\n%s\n"%[tl.id, tl.user.screen_name, tl.text]
 		end
+	end
+elsif (command == "mentions")
+	Twitter.mentions.each do |tl|
+		puts "%s %s\n%s\n"%[tl.id, tl.user.screen_name, tl.text]
 	end
 elsif (command == "tw")
 	Twitter.update(ARGV.shift.chomp)
 elsif (command == "retweet" or command == "rt")
 	Twitter.retweet(ARGV.shift.chomp)
 else
-	Twitter.update(command)
+	puts "ERROR!"
 end
 
